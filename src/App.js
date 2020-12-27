adb904a50de393971c1a9e097e4185712d72f63c
Aymane Hrouch <jo.aymane@gmail.com>
2020-12-27 12:08:48 +0100

    show and hide nav elements based on auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React, { Component } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
 import Locked from "./components/locked";
 import NavigationBar from "./components/navBar";
 import MyForm from "./components/myForm";
@@ -7,35 +9,49 @@ import NotFound from "./components/not-found";
 import Journal from "./components/journal";
 import Journals from "./components/journals";
 import Random from "./components/random";
+import Logout from './components/logout';
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
 import LoginForm from "./components/loginForm";
-import RegisterForm from "./components/registerForm";
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <NavigationBar />
-      <ToastContainer
-        position="bottom-center"
-        autoClose={3000}
-        pauseOnHover={false}
-        pauseOnFocusLoss={false}
-      />
-      <Switch>
-        <Route path="/journals/:id" component={Journal} />
-        <Route path="/journals" component={Journals} />
-        <Route path="/random" component={Random} />
-        <Route path="/new" component={MyForm} />
-        <Route path="/login" component={LoginForm} />
-        {/* <Route path="/register" component={RegisterForm} /> */}
-        <Route path="/not-found" component={NotFound} />
-        <Route path="/locked" component={Locked} />
-        <Redirect from="/" exact to="/new" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({user});
+    } catch(ex) {}
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div>
+        <NavigationBar user={user} />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={3000}
+          pauseOnHover={false}
+          pauseOnFocusLoss={false}
+        />
+        <Switch>
+          <Route path="/journals/:id" component={Journal} />
+          <Route path="/journals" component={Journals} />
+          <Route path="/random" component={Random} />
+          <Route path="/new" component={MyForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/logout" component={Logout} />
+          {/* <Route path="/register" component={RegisterForm} /> */}
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/locked" component={Locked} />
+          <Redirect from="/" exact to="/new" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,39 +2,55 @@ import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink, Link } from "react-router-dom";
 
-class NavigationBar extends Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand as={Link} to="/">
-          overThinker
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="w-100">
-            <Nav.Link as={NavLink} exact to="/journals">
-              Journals
-            </Nav.Link>
-            <Nav.Link as={NavLink} exact to="/random">
-              random
-            </Nav.Link>
-            <Nav.Link as={NavLink} exact to="/locked">
-              Locked
-            </Nav.Link>
-            <Nav.Link as={NavLink} exact to="/login">
-              Login
-            </Nav.Link>
-            {/* <Nav.Link as={NavLink} exact to="/register">
-              Register
-            </Nav.Link> */}
-            <Nav.Link as={NavLink} exact to="/profile" className="ml-md-auto">
-              Aymane Hrouch
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
-}
+const NavigationBar = ({ user }) => {
+  console.log(user);
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        overThinker
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="w-100">
+          {user && (
+            <React.Fragment>
+              <Nav.Link as={NavLink} exact to="/journals">
+                Journals
+              </Nav.Link>
+              <Nav.Link as={NavLink} exact to="/random">
+                random
+              </Nav.Link>
+              <Nav.Link as={NavLink} exact to="/locked">
+                Locked
+              </Nav.Link>
+              <Nav.Link as={NavLink} exact to="/logout">
+                Logout
+              </Nav.Link>
+              <Nav.Link as={NavLink} exact to="/profile" className="ml-md-auto">
+                {user ? user.name : ""}
+              </Nav.Link>
+            </React.Fragment>
+          )}
+          {!user && (
+            <React.Fragment>
+              <Nav.Link
+                as={NavLink}
+                exact
+                to="/register"
+                className="ml-md-auto"
+              >
+                Register
+              </Nav.Link>
+              <Nav.Link as={NavLink} exact to="/login">
+                Login
+              </Nav.Link>
+            </React.Fragment>
+          )}
+          {/*  */}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default NavigationBar;
