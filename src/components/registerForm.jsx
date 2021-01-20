cb9d86039489c6f439da48f254e90d6f2404bd5d
Aymane Hrouch <jo.aymane@gmail.com>
2021-01-20 19:21:45 +0000

    add register form

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Container, Form, Button } from "react-bootstrap";
-import Input from "./input";
 import Joi from "joi-browser";
 import auth from "./../services/authService";
+import StandardForm from "./common/standardForm";
+import { toast } from "react-toastify";
 
-class RegisterForm extends Component {
+class LoginForm extends StandardForm {
   state = {
-    account: {
+    data: {
+      name: "",
       email: "",
       password: "",
     },
@@ -14,44 +16,18 @@ class RegisterForm extends Component {
   };
 
   schema = {
+    name: Joi.string().required().max(30).min(3).label("Name"),
     email: Joi.string().email().required().label("Email"),
-    password: Joi.string().required().label("Password"),
-  };
-
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null; // if there's no error
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-
-    return errors;
-  };
-
-  validateProprety = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-
-    return error ? error.details[0].message : null;
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
-    this.doSubmit();
+    password: Joi.string().required().min(5).label("Password"),
   };
 
   doSubmit = async () => {
     try {
-      const { account } = this.state;
-      await auth.login(account.email, account.password);
-      window.location = "/";
+      const { data } = this.state;
+      await auth.register(data.name, data.email, data.password);
+      toast.info("User registred with success!!");
+      toast.info("You can now login");
+      this.props.history.push("/login");
     } catch (ex) {
       const errors = { ...this.state.errors };
       console.log(ex.response);
@@ -60,48 +36,21 @@ class RegisterForm extends Component {
     }
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProprety(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
     return (
       <Container>
-        <h1>REGISTER FORM</h1>
         <Form
           className="col-md-3 mt-4 ml-auto mr-auto"
           onSubmit={this.handleSubmit}
         >
-          <Input
-            name="email"
-            label="Email"
-            value={account.email}
-            onChange={this.handleChange}
-            error={errors.email}
-          />
-          <Input
-            name="password"
-            label="Password"
-            value={account.password}
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <Button disabled={this.validate()} variant="primary" type="submit">
-            Login
-          </Button>
+          {this.renderInput("name", "Name", "text")}
+          {this.renderInput("email", "Email", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Register")}
         </Form>
       </Container>
     );
   }
 }
 
-export default RegisterForm;
+export default LoginForm;
