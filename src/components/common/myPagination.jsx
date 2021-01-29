8df2de5a0de95d68e627e687b6db369a4688b913
Aymane Hrouch <jo.aymane@gmail.com>
2021-01-29 21:27:13 +0000

    hide previous/next buttons dynamically

--- src/components/common/myPagination.jsx
+++ src/components/common/myPagination.jsx
@@ -2,30 +2,32 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const StyledUl = styled.ul`
-  cursor: pointer;
-  & li {
-    background: ${({ theme }) => theme.card};
-    border: ${({ theme }) => theme.cardBorder};
-  }
-
-  & li:first-child {
-    border-radius: 15px 0 0 15px;
-  }
-
-  & li:last-child {
-    border-radius: 0 15px 15px 0;
-  }
-`;
-
 class MyPagination extends Component {
   render() {
     const { currentPage, journalsLength, onChange } = this.props;
+
+    const StyledUl = styled.ul`
+      cursor: pointer;
+      & li {
+        background: ${({ theme }) => theme.card};
+        border: ${({ theme }) => theme.cardBorder};
+      }
+
+      & li:first-child {
+        border-radius: 15px 0 0 15px;
+        display: ${currentPage <= 1 && "none"};
+      }
+
+      & li:last-child {
+        border-radius: 0 15px 15px 0;
+        display: ${journalsLength === 0 && "none"}
+      }
+    `;
+
     return (
       <StyledUl className="pagination">
         <li
           className="page-link"
-          disabled={currentPage <= 1}
           onClick={() => onChange("prev")}
         >
           <i className="fa fa-chevron-left pr-1" aria-hidden="true"></i>
@@ -33,7 +35,6 @@ class MyPagination extends Component {
         </li>
         <li
           className="page-link"
-          disabled={journalsLength === 0}
           onClick={() => onChange("next")}
         >
           Next <i className="fa fa-chevron-right pl-1" aria-hidden="true"></i>
