590b2fe69fddb15ac5cc737d9188b7b952525970
Aymane Hrouch <jo.aymane@gmail.com>
2021-01-29 12:51:23 +0000

    use hooks

--- src/App.js
+++ src/App.js
@@ -13,63 +13,66 @@ import RegisterForm from "./components/registerForm";
 import LoginForm from "./components/loginForm";
 import ProtectedRoute from "./components/common/protectedRoute";
 import Profile from "./components/profile";
-import Settings from './components/settings';
+import Settings from "./components/settings";
 import auth from "./services/authService";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
-class App extends Component {
-  state = {};
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/GlobalStyle";
+import { lightTheme, darkTheme } from "./components/Themes";
+import { useDarkMode } from "./useDarkMode";
+import { useState, useEffect } from "react";
 
-  componentDidMount() {
-    const user = auth.getCurrentUser();
-    this.setState({ user });
-  }
+const App = () => {
+  const [user, setUser] = useState();
+  const [theme, setTheme] = useState("light")
 
-  handleChangeName = name => this.setState({ name });
+  useEffect(() => {
+    setUser(auth.getCurrentUser());
+  }, []);
 
-  render() {
-    const { user } = this.state;
-    
-    return (
-      <div>
-        <NavigationBar user={user} />
-        <ToastContainer
-          position="bottom-center"
-          autoClose={3000}
-          pauseOnHover={false}
-          pauseOnFocusLoss={false}
+  // handleChangeName = name => this.setState({ name });
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <NavigationBar user={user} />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+      />
+      <Switch>
+        <ProtectedRoute path="/journals/:id" component={Journal} />
+        <ProtectedRoute path="/journals" component={Journals} />
+        <ProtectedRoute path="/random" component={Random} />
+        <ProtectedRoute path="/new" component={MyForm} />
+        <ProtectedRoute path="/settings" component={Settings} />
+        <ProtectedRoute path="/profile" component={Profile} />
+        <Route
+          path="/login"
+          render={props => {
+            if (user) return <Redirect to="/new" />;
+            return <LoginForm />;
+          }}
         />
-        <Switch>
-          <ProtectedRoute path="/journals/:id" component={Journal} />
-          <ProtectedRoute path="/journals" component={Journals} />
-          <ProtectedRoute path="/random" component={Random} />
-          <ProtectedRoute path="/new" component={MyForm} />
-          <ProtectedRoute path="/settings" component={Settings} />
-          <ProtectedRoute path="/profile" component={Profile} />
-          <Route
-            path="/login"
-            render={props => {
-              if (auth.getCurrentUser()) return <Redirect to="/new" />;
-              return <LoginForm />;
-            }}
-          />
-          <Route path="/logout" component={Logout} />
-          <Route
-            path="/register"
-            render={props => {
-              if (auth.getCurrentUser()) return <Redirect to="/new" />;
-              return <RegisterForm {...props} />;
-            }}
-          />
-          <Route path="/not-found" component={NotFound} />
-          <ProtectedRoute path="/locked" component={Locked} />
-          <Redirect from="/" exact to="/new" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </div>
-    );
-  }
-}
+        <Route path="/logout" component={Logout} />
+        <Route
+          path="/register"
+          render={props => {
+            if (user) return <Redirect to="/new" />;
+            return <RegisterForm {...props} />;
+          }}
+        />
+        <Route path="/not-found" component={NotFound} />
+        <ProtectedRoute path="/locked" component={Locked} />
+        <Redirect from="/" exact to="/new" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </ThemeProvider>
+  );
+};
 
 export default App;
