e9392771f61468e72ef2f4470ded33b172354c7d
Aymane Hrouch <jo.aymane@gmail.com>
2021-02-14 04:18:35 +0000

    use sessionStorage instead of localStorage

--- src/services/authService.js
+++ src/services/authService.js
@@ -11,20 +11,20 @@ export async function register(name, email, password) {
 
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndpoint, { email, password });
-  localStorage.setItem("token", jwt);
+  sessionStorage.setItem("token", jwt);
 }
 
 export function logout() {
-  localStorage.removeItem("token");
+  sessionStorage.removeItem("token");
 }
 
 export function getJwt() {
-  return localStorage.getItem("token");
+  return sessionStorage.getItem("token");
 }
 
 export function getCurrentUser() {
   try {
-    const jwt = localStorage.getItem("token");
+    const jwt = sessionStorage.getItem("token");
     return jwtDecode(jwt);
   } catch (ex) {
     return null;
